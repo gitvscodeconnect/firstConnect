5eab420843774d4914163a5c8b4e6b708dd36f9a
K <qi.chi.dong@gmail.com>
2024-07-07 16:00:08 +0300

    publish the first branch from personal computer add 2 more lines, check success save in the github repo

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 ﻿print("hi")
 print("how are you")
+#here I can edit
+#I further print more stuff
+#I wish I can turn off the frequent fetch
+print("new 3rd line just to check")
+print("in the github website saves it")
